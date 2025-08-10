4b302522b3c1f07ab4103bae6539438a6135838b
mark <mark.mekhayl@gmail.com>
2025-08-10 21:44:26 +1000

    Initial commit from Create Next App

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'logo.clearbit.com' },
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-    ],
-  },
-  async rewrites() {
-    return [
-      { source: '/sell-:brand', destination: '/brands/:brand' },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
